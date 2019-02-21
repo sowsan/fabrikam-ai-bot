4e28a5de02a17a3ffaec9e16bd349c982677893a
Sowmyan Soman Chullikkattil <sowmyans@microsoft.com>
2019-02-21 17:15:27 -0500

    Add project files.

---
+++ Fabrikam.AI.Bot/Fabrikam.AI.BotState.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace Fabrikam.AI.Bot
+{
+    public class Fabrikam.AI.BotState : DialogState
+    {
+    }
+}
